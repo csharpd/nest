3fb7ae1fab193f989a10f6321bd1f497e31422e3
Chloe Donegan <chloedonegan@gmail.com>
2014-09-30 14:30:07 +0100

    max temperature and power saving mode functionality added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,33 +9,64 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it("should begin with PSM on", function(){
-    expect(thermostat.powersavingMode).toBe(true);
+  it("should be begin with PSM on", function() {
+    expect(thermostat.powerSavingMode).toBe(true);
   });
 
-  describe("increasing the temperature", function() {
-    it("increases if temperature < 25", function () {
-      thermostat.increaseTemperature();
-      expect(thermostat.temperature).toEqual(21);
-    });
+  describe('maximum temperature', function(){
+    it('is 25 degrees with PSM on', function() {
+      expect(thermostat.maximumTemperature()).toEqual(25);
+    })
 
-    it("does not increase if the temperature >= 25", function() {
-      thermostat.temperature = 25;
-      thermostat.increaseTemperature();
-      expect(thermostat.temperature).toEqual(25);
-    });
+    it('is 32 degrees with PSM on', function() {
+      thermostat.powerSavingMode = false;
+      expect(thermostat.maximumTemperature()).toEqual(32);
+    })
   });
 
-  describe("decreasing the temperature", function(){
-    it("decreases if temperature >= 10", function (){
-      thermostat.decreaseTemperature();
-      expect(thermostat.temperature).toEqual(19);
+   describe('increasing the temperature', function() {
+    describe('PSM off', function(){
+      beforeEach(function() {
+        thermostat.powerSavingMode = false;
+      });
+
+      it("increases if temperature < 32", function () {
+        thermostat.temperature = 25
+        thermostat.increaseTemperature();
+        expect(thermostat.temperature).toEqual(26);
+      });
+
+      it("does not increase if the temperature >= 32", function() {
+        thermostat.temperature = 32;
+        thermostat.increaseTemperature();
+        expect(thermostat.temperature).toEqual(32);
+      });
     });
 
-    it("does not decrease if the temperature < 10 ",function() {
-      thermostat.temperature = 10
-      thermostat.decreaseTemperature();
-      expect(thermostat.temperature).toEqual(10);
+    describe("with PSM on", function() {
+      it("increases if temperature < 25", function () {
+        thermostat.increaseTemperature();
+        expect(thermostat.temperature).toEqual(21);
+      });
+
+      it("does not increase if the temperature >= 25", function() {
+        thermostat.temperature = 25;
+        thermostat.increaseTemperature();
+        expect(thermostat.temperature).toEqual(25);
+      });
+    });
+
+    describe("decreasing the temperature", function(){
+      it("decreases if temperature >= 10", function (){
+        thermostat.decreaseTemperature();
+        expect(thermostat.temperature).toEqual(19);
+      });
+
+      it("does not decrease if the temperature < 10 ",function() {
+        thermostat.temperature = 10
+        thermostat.decreaseTemperature();
+        expect(thermostat.temperature).toEqual(10);
+      });
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,18 @@
 function Thermostat() {
   this.temperature = 20;
-  this.powersavingMode = true;
+  this.powerSavingMode = true;
 }
 
 Thermostat.prototype.increaseTemperature = function() {
-  if(this.temperature < 25) this.temperature += 1;
+  if(this.temperature < this.maximumTemperature()) { this.temperature += 1;
+  }
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
   if(this.temperature > 10) this.temperature -= 1;
 };
+
+Thermostat.prototype.maximumTemperature = function() {
+  return (this.powerSavingMode) ? 25 : 32;
+};
+
