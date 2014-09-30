bfa50d5a10be6e9a68c7d69104fea7dd8d6c67a8
Chloe Donegan <chloedonegan@gmail.com>
2014-09-30 13:28:09 +0100

    can increase the thermostat temp up until 25 degrees

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,17 @@ describe("Thermostat", function() {
   it("should be equal to 20", function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  describe("increasing the temperature", function() {
+    it("increases if temperature < 25", function () {
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it("does not increase if the temperature >= 25", function() {
+      thermostat.temperature = 25;
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(25);
+    });
+  });
 });
